454f4493a87b2e5c3b599df02156951fdd210928
araujo <theriancirculo@protonmail.com>
2025-07-14 15:49:59 -0300

    del: Files moved for backend dir

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/dev-araujo/go-healthchecker
-
-go 1.24.4
--- main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/dev-araujo/go-healthchecker/api"
-
-func main() {
-	server := api.NewServer()
-	server.Start(":8080")
-}
