a3a643e772f6da7f4e5630afd9edf3b9372349ac
araujo <theriancirculo@protonmail.com>
2025-07-15 14:49:09 -0300

    feat(frontend): Improve ui and add dev environment

--- frontend/src/app/features/health-check/services/api.service.ts
+++ frontend/src/app/features/health-check/services/api.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ApiResponse } from '../models/health-check.model';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private readonly apiUrl = 'https://go-healthchecker.onrender.com/check';
+  private readonly apiUrl = `${environment.apiUrl}/check`;
 
   constructor(private http: HttpClient) { }
 
