63d0cab40631d8f4fcf8aae9f2ab7a10adb2b1f3
araujo <theriancirculo@protonmail.com>
2025-08-09 11:05:17 -0300

    refactor: Improve middleWare logic

--- backend/internal/api/server.go
+++ backend/internal/api/server.go
@@ -21,24 +21,9 @@ func NewServer(checker healthcheck.Checker) *Server {
 }
 
 func (s *Server) Router() http.Handler {
-	return corsMiddleware(s.router)
+	return CorsMiddleware(s.router)
 }
 
 func (s *Server) registerRoutes() {
 	s.router.HandleFunc("/check", s.handleCheck())
-}
-
-func corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
 }
